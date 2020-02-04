d7c21ff263905f509160215da5891dea8ff004be
Mincky <54250862+mickers13@users.noreply.github.com>
2020-02-04 20:11:11 +0100

    opdracht 1.2 af

--- Structured-Programming/1.2 formatief.cpp
+++ Structured-Programming/1.2 formatief.cpp
@@ -7,8 +7,17 @@ string input_text(){
     return a;
 }
 int main() {
-    cout<<input_text();
+    string texta = input_text();
+    string textb = input_text();
 
+    for( int i=0; i<=texta.length() ; i++ ){
+        if(texta[i] != textb[i]){
+            cout<<"ohje, vanaf de "<<i-1<<"de character zijn volgende zinnen niet hetzelfde! :"<<endl;
+            cout<<texta<<endl;
+            cout<<textb<<endl;
+        }
+
+    }
 
 }
 
