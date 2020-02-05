7d1923c9f78418fe677f9752fd768d25de5b826f
Mincky <54250862+mickers13@users.noreply.github.com>
2020-02-05 20:26:31 +0100

    1.4 beter gemaakt, c++ is lastig.

--- Structured-Programming/1.4 formatief.cpp
+++ Structured-Programming/1.4 formatief.cpp
@@ -2,24 +2,16 @@
 #include <string>
 #include <vector>
 #include <cmath>
-#include <algorithm>
 
 using namespace std;
-bool palindroomCheck(string woord){
-    string normaal;
-    reverse(woord.begin(),woord.end());
-    if (normaal == woord){
-        cout<< woord<<endl;
-        cout<<"jaaaaaaa hoor. Dit is palindroom.";
-    }   else{
-        cout<< woord<<endl;
-        cout<<"Nee helaas. Geen palindroom, probeer het een andere keer weer!";
-    }
-    
-
-
-    ;
-    
+void palindroomCheck(string woord){
+    string normaal = woord;
+    // gevonden via stackoverflow, erg mooie manier om een string te reversen. Kon geen library vinden die dit beter kon, geen enkele library nodig. https://stackoverflow.com/questions/4951796/how-to-reverse-an-stdstring
+    string reversed(normaal.rbegin(), normaal.rend());
+    if (normaal.compare(reversed) == 0)
+        cout<<"Dit woord is wel een palindroom.";
+    else
+        cout<<"Dit woord is geen palindroom.";
 }
 
 int main(){
