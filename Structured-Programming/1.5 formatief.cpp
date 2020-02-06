d7e7a9f4dd1feb16f550a1a8b4cd441fe3032129
Mincky <54250862+mickers13@users.noreply.github.com>
2020-02-06 12:42:39 +0100

    finito

--- Structured-Programming/1.5 formatief.cpp
+++ Structured-Programming/1.5 formatief.cpp
@@ -23,33 +23,23 @@ void sorteer(vector<int>& list) {
 }
 
 void sorteer_hulp(vector<int> &list, size_t i, size_t j){
-	// blok grootte 1 is al gesorteerd
 	if (j - i <= 1)
 		return;
-	// splits de lijst op in 3 delen, van links naar rechts:
-	// kleiner-gelijk aan pivot, pivot, groter dan pivot
 	size_t piv = split(list, i, j, i);
-	// sorteer links en rechts
 	sorteer_hulp(list, i, piv);
 	sorteer_hulp(list, piv + 1, j);
 }
 
 size_t split(vector<int> &list, size_t i, size_t j, size_t piv) {
-	// j begint als eerste waarde BUITEN de lijst. Haal j naar binnen.
-	// j wijst nu naar de laaste waarde in lijst
 	j -= 1;
-	// zolang i en j niet op dezelfde plek staan (de pivot!)
 	while(j - i > 0) {
 		if (list[i + 1] <= list[i]) {
-			// groei linker sectie
 			swap(list[i], list[i + 1]);
 			i++;
 		} else {
-			// groei rechter sectie
 			swap(list[j], list[i + 1]);
 			j--;
 		}
 	} 
-	// geef de positie van de pivot terug
 	return i;
 }
