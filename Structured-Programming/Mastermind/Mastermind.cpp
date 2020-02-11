ebd5711768a4361747fac7b5b4c10429ffadef54
Mincky <54250862+mickers13@users.noreply.github.com>
2020-02-11 20:36:43 +0100

    Kleur en basis opzet is klaar Mastermind
    Kan 6 verschillende kleuren pakken, kan dingen overschrijven in commandline, en verder een data structuur opgezet voor de regels. Verder print hij het spel mastermind netjes in passende regels.

--- Structured-Programming/Mastermind/Mastermind.cpp
+++ Structured-Programming/Mastermind/Mastermind.cpp
@@ -1,21 +1,128 @@
+#include <vector>
 #include <iostream>
-using namespace std;
+#include <string>
+#include <windows.h>
+using std::vector;
+using std::string;
+using std::endl;
+using std::cout;
+/*Ik doe een paar gekke dingen, die ik zelf erg leuk vond om uit te zoeken om een leuke Commandline game te maken ( kleurtjes in CMD enzo. ). paar voorbeelden voor de bronnen:
+https://en.wikipedia.org/wiki/ANSI_escape_code
+
+
+
+*/
+
+
+
+
+/*Globale settings voor het hele programma, waar veel naar gekeken gaat worden. 
+g_* ter verduidelijking dat het een global is. 
+Dit is voor de syntax niet nodig maar wel fijn, aangezien je soms de :: over het hoofd ziet.*/
+int g_amountOfColumns;
+int g_amountOfColours;
+
+
+void startSetup(int columns = 4, int colours = 6){
+    ::g_amountOfColumns = columns;
+    ::g_amountOfColours = colours;
+
+}
+
+void colorprint(string character,char color){
+    switch(color){
+        case'R': 
+            cout<<"\033[40;101m";
+            cout<<character;
+            cout<<"\033[39;49m";
+            break;
+
+        case'G':
+            cout<<"\033[40;102m";
+            cout<<character;
+            cout<<"\033[39;49m";
+            break;
+
+        case'B':
+            cout<<"\033[40;104m";
+            cout<<character;
+            cout<<"\033[39;49m";
+            break;
+
+        case'C':        
+            cout<<"\033[40;106m";
+            cout<<character;
+            cout<<"\033[39;49m";
+            break;
+
+        case'M':
+            cout<<"\033[40;105m";
+            cout<<character;
+            cout<<"\033[39;49m";
+            break;
+        case'Y':
+            cout<<"\033[40;103m";
+            cout<<character;
+            cout<<"\033[39;49m";
+            break;
+    }
+}
+
+void printinrow(vector<vector<int>>data){
+    cout<<"\033[F|";
+        for(int i = 0; i<g_amountOfColumns; i++){
+            
+            colorprint("O",'R');
+        }
+        cout<<"|";
+        for(int i = 0; i<g_amountOfColumns; i++){
+            cout<<" ";
+        }cout<<"|";
+        for(int i = 0; i<g_amountOfColumns; i++){
+            cout<<" ";
+        }cout<<"|";
+}
+
+void outrow(vector<vector<int>>data){
+    // Print text.
+        cout<<"|Code:";
+        
+        for(int i = 0; i<g_amountOfColumns; i++){
+            cout<<" ";
+        }
+
+        cout<<"Clues|\n";
+
+    // Print a line.
+        cout<<"|";
+        for(int i = 0; i<g_amountOfColumns; i++){
+            cout<<"=";
+        }cout<<"|";
+        for(int i = 0; i<g_amountOfColumns; i++){
+            cout<<" ";
+        }cout<<"|";
+        for(int i = 0; i<g_amountOfColumns; i++){
+            cout<<"=";
+        }
+        cout<<"|                                                                             \n";
+    printinrow(data);
+
+    
+
+}
+void overwrite(string phrase){
+    //overschrijf de vorige lijn, met een nieuwe + veel spaties om volledig te overschrijven
+    cout<<"\033[F\r"<<phrase<<"                                                                  \n";
+}
+
 int main() {
-    int breedte_pyramide = 0;
-    int breedteCounter = 0;
-    cout<<"Hoe groot wil je je pyramide hebben? Voer alleen cijfers in en geen andere symbolen!";
-    cin>>breedte_pyramide;
-    for(int breedteCounter = 0; breedteCounter<=breedte_pyramide; breedteCounter++){
-        cout<<endl;
-        for(int lengteCounter = 0; lengteCounter<=breedteCounter; lengteCounter++){
-            cout<<"X";
-        }
-    }
-    for(int breedteCounter2 = breedte_pyramide-1; breedteCounter2>0; breedteCounter2--){
-        cout<<endl;
-        for(int lengteCounter = 0; lengteCounter<=breedteCounter2; lengteCounter++){
-            cout<<"X";
-        }
-    }
+    system("CLS");
+    char test;
+    vector<vector<int>> data;
+    startSetup(4,6);
+    outrow(data);
+
+    
+    
 }
 
---
+++ Structured-Programming/Mastermind/cmdtextanimationtest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+int main(){
+    // voorbeeldje van sleep en rewriten van je print. Leuk voor laadbalken, en mastermind!
+    std::cout<<"\r test 1234";
+    Sleep(300);
+    std::cout<<"\r test 12345";
+    Sleep(300);
+    std::cout<<"\r test 123456";
+
+
+    
+
+}
